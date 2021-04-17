225405cd826e8d50fc02c6e02de4e3a310c6a12f
CYIMANA Faisal <cyimanafaisal@gmail.com>
2021-04-17 17:35:49 +0200

    Played with cashing and plugin to make dynamic inclusion of script tag name to the template

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,16 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
   mode: "development",
   entry: "./src/index.js",
   output: {
-    filename: "main.js",
+    filename: "main-[contenthash].js",
     path: path.resolve(__dirname, 'dist')
+
   },
+  plugins:[new HtmlWebpackPlugin({
+    template:"index.html"
+  })],
   module:{
     rules:[
       {
@@ -18,4 +23,5 @@ module.exports = {
       }
     ]
   }
+
 };
